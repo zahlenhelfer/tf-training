1ab6dca79ab00d18b36a6396ddcb8969d7d856ee
Marcus Ross <marcusross@Marcuss-Air.fritz.box>
2020-06-05 15:32:41 +0200

    module and user_data extracted

--- aws.tf
+++ aws.tf
@@ -3,3 +3,11 @@ provider "aws" {
   region     = "eu-central-1"
   version    = "~> 2.65"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "zh-tf-state"
+    key    = "meinprojekt.tfstate"
+    region = "eu-central-1"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,19 +1,30 @@
 # EC2 Instanz erstellen
 resource "aws_instance" "web" {
   ami           = "ami-0a02ee601d742e89f"
-  instance_type = "t2.micro"
-  count         = 3
-  user_data     = <<-EOF
-              #!/bin/bash
-              yum install httpd -y
-              /sbin/chkconfig --levels 235 httpd on
-              service httpd start
-              instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-              region=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
-              echo "<h1>$instanceId from $region</h1>" > /var/www/html/index.html              
-              EOF
+  instance_type = var.server_type
+  count         = var.server_count
+  user_data     = file("./user_data.sh")
 
   tags = {
     Name = "Instanz ${count.index}"
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "sldemo-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
